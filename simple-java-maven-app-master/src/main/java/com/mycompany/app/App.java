96d30a71a51fb9b396459701203fe408f0acf43a
thatsenthil <36446732+thatsenthil@users.noreply.github.com>
2019-05-02 09:44:16 +0100

    Update App.java

--- simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
+++ simple-java-maven-app-master/src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Welcome to Azure World by Senthilmurugan";
 
     public App() {}
 
